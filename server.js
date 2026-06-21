08e93fa430b049997feaf71fc1b1c3e71dc29d16
xettrisaimon13-tech <xettrisaimon13@gmail.com>
2026-06-20 18:51:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const WebSocket = require("ws");
-const http = require("http");
+const WebSocket = require('ws');
+const http = require('http');
 
 const server = http.createServer();
 const wss = new WebSocket.Server({ server });
@@ -7,59 +7,36 @@ const wss = new WebSocket.Server({ server });
 let players = {};
 let nextId = 1;
 
-wss.on("connection", (ws) => {
-    const id = nextId++;
-    players[id] = ws;
+wss.on('connection', (ws) => {
+    const playerId = nextId++;
+    players[playerId] = ws;
 
-    console.log("Player joined:", id);
+    const existingIds = Object.keys(players)
+        .map(id => parseInt(id))
+        .filter(id => id !== playerId);
 
-    // send welcome + existing players
-    ws.send(JSON.stringify({
-        type: "welcome",
-        id: id,
-        players: Object.keys(players).map(x => parseInt(x))
-    }));
+    ws.send(JSON.stringify({ type: "welcome", id: playerId, existing_players: existingIds }));
+    broadcast({ type: "player_joined", id: playerId }, playerId);
 
-    broadcast({
-        type: "player_joined",
-        id: id
-    }, id);
-
-    ws.on("message", (msg) => {
-        try {
-            const data = JSON.parse(msg);
-
-            broadcast({
-                type: "data",
-                from: id,
-                payload: data
-            }, id);
-
-        } catch (e) {}
+    ws.on('message', (msg) => {
+        const data = JSON.parse(msg);
+        broadcast({ type: "data", from: playerId, payload: data }, playerId);
     });
 
-    ws.on("close", () => {
-        delete players[id];
-
-        broadcast({
-            type: "player_left",
-            id: id
-        });
+    ws.on('close', () => {
+        delete players[playerId];
+        broadcast({ type: "player_left", id: playerId }, playerId);
     });
 });
 
-function broadcast(data, exclude = null) {
+function broadcast(data, excludeId = null) {
     const msg = JSON.stringify(data);
-
     for (const id in players) {
-        if (exclude && id == exclude) continue;
-
-        const c = players[id];
-        if (c && c.readyState === WebSocket.OPEN) {
-            c.send(msg);
+        if (id != excludeId && players[id].readyState === WebSocket.OPEN) {
+            players[id].send(msg);
         }
     }
 }
 
 const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log("Server running"));
+server.listen(PORT, () => console.log(`Server on port ${PORT}`));
